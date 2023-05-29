0f4a5880b99f638327ed4849c92971684cc5843d
chenghuaWang <2923277184@qq.com>
2023-05-29 13:00:06 +0800

    [update] lexer 50%

--- include/nncv/compiler/Frontend/TenLangLexer.hpp
+++ include/nncv/compiler/Frontend/TenLangLexer.hpp
@@ -17,6 +17,12 @@
 #include <memory>
 #include <string>
 
+#include <tuple>
+#include <map>
+
+#include <iostream>
+#include <fstream>
+
 #include <stdint.h>
 
 namespace nncv {
@@ -27,6 +33,8 @@ namespace fronted {
 // Structure definition a location in a file.
 //===----------------------------------------------------------------------===//
 struct Location {
+  Location() = default;
+  Location(std::shared_ptr<std::string> _Fn, int L, int C) : FileName(_Fn), Line(L), Col(C) {}
   std::shared_ptr<std::string> FileName;  ///< filename.
   int Line;                               ///< line number.
   int Col;                                ///< column number.
@@ -54,6 +62,8 @@ enum AutoTenTokenType : int {
 enum AutoTenTokenValue : int {
   // Auto Tenser lang v 0.0.1
 
+  kUnused = -1024,
+
   // Control
   kAnd = -1,      /// logical &&
   kOr = -2,       /// logical ||
@@ -66,6 +76,12 @@ enum AutoTenTokenValue : int {
   kFunc = -9,     /// Function define
   kImpl = -10,    /// The Implementation of funcs for Structure
   kReturn = -11,  /// Return something
+  kXor = -66,
+  kMod = -67,
+  kShl = -68,  /// Shift left
+  kShr = -69,  /// Shift right
+  kNil = -70,
+  kReturnValue = -71,
 
   // Built in types
   kTensor = -12,
@@ -80,6 +96,7 @@ enum AutoTenTokenValue : int {
   kStruct = -21,
   kFile = -64,
   kVar = -65,  /// Type is auto inferenced.
+  kInt64 = -72,
 
   // I/O
   kWrite = -22,
@@ -93,6 +110,8 @@ enum AutoTenTokenValue : int {
   kBracketClose = '}',
   kSbracketOpen = '[',
   kSbracketClose = ']',
+  kAngleBracketOpen = '<',
+  kAngleBracketClose = '>',
   kAt = '@',
   kComma = ',',
   kColon = ':',
@@ -114,10 +133,145 @@ enum AutoTenTokenValue : int {
 };
 
 struct AutoTenToken {
-  inline AutoTenToken();
-  inline AutoTenToken(AutoTenTokenType type, AutoTenTokenValue value, const Location& location,
-                      const std::string& name, int symbolPrecedence) {}
+  inline AutoTenToken() = default;
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      const std::string& Name, int SymbolPrecedence)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(SymbolPrecedence) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      const std::string& StrValue, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kString(StrValue) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      int8_t Int8Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kI8(Int8Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      int16_t Int16Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kI16(Int16Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      int32_t Int32Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kI32(Int32Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      int64_t Int64Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kI64(Int64Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      float Float32Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kF32(Float32Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      double Float64Value, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kF64(Float64Value) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      bool BoolValue, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kBool(BoolValue) {}
+  inline AutoTenToken(AutoTenTokenType Type, AutoTenTokenValue Value, const Location& Location,
+                      char CharValue, std::string Name)
+      : m_Type(Type),
+        m_Value(Value),
+        m_Location(Location),
+        m_Name(Name),
+        m_SymbolPrecedence(-1),
+        m_kChar(CharValue) {}
+
   inline AutoTenTokenType GetTokenType() const { return m_Type; }
+  inline AutoTenTokenValue GetTokenValue() const { return m_Value; }
+  inline const Location& GetTokenLocation() const { return m_Location; }
+  inline std::string GetTokenName() const { return m_Name; }
+
+  inline int GetSymbolPrecedence() const { return m_SymbolPrecedence; }
+
+  inline int8_t GetInt8Value() const { return m_kI8; }
+  inline int8_t GetInt16Value() const { return m_kI16; }
+  inline int8_t GetInt32Value() const { return m_kI32; }
+  inline int8_t GetInt64Value() const { return m_kI64; }
+  inline float GetFloat32Value() const { return m_kF32; }
+  inline double GetFloat64Value() const { return m_kF64; }
+  inline std::string GetStringValue() const { return m_kString; }
+  inline bool GetBoolValue() const { return m_kBool; }
+  inline char GetCharValue() const { return m_kChar; }
+
+  inline void DebugDump(std::ostream& out = std::cout) const {}
+
+  inline std::string GetIdentifierName() const { return m_Name; }
+
+  inline std::string TokenTypeDescription() const {
+    std::string ret;
+
+    switch (m_Type) {
+      case AutoTenTokenType::kTypeInteger: {
+        switch (m_Value) {
+          case AutoTenTokenValue::kInt8: ret = "integer(8 bits)"; break;
+          case AutoTenTokenValue::kInt16: ret = "integer(16 bits)"; break;
+          case AutoTenTokenValue::kInt32: ret = "integer(32 bits)"; break;
+          case AutoTenTokenValue::kInt64: ret = "integer(64 bits)"; break;
+          default: break;
+        }
+      }
+      case AutoTenTokenType::kTypeReal: {
+        switch (m_Value) {
+          case AutoTenTokenValue::kFloat32: ret = "float(32 bits)"; break;
+          case AutoTenTokenValue::kFloat64: ret = "float(64 bits)"; break;
+          default: break;
+        }
+      }
+      case AutoTenTokenType::kTypeTensor: ret = "tensor"; break;
+      case AutoTenTokenType::kTypeBool: ret = "boolean"; break;
+      case AutoTenTokenType::kTypeChar: ret = "char"; break;
+      case AutoTenTokenType::kTypeString: ret = "string"; break;
+      case AutoTenTokenType::kIdentifier: ret = "identifier"; break;
+      case AutoTenTokenType::kKeywords: ret = "keywords"; break;
+      case AutoTenTokenType::kOperators: ret = "operators"; break;
+      case AutoTenTokenType::kDelimiter: ret = "delimiter"; break;
+      case AutoTenTokenType::kEof: ret = "eof"; break;
+      case AutoTenTokenType::kTypeUnknow: ret = "unknown"; break;
+      default: break;
+    }
+    return ret;
+  }
+  inline std::string ToString() const {
+    return std::string("Token Type: " + TokenTypeDescription() + "Token Name: " + m_Name);
+  }
 
  private:
   AutoTenTokenType m_Type;
@@ -134,14 +288,95 @@ struct AutoTenToken {
   float m_kF32;
   double m_kF64;
   std::string m_kString;
+  bool m_kBool;
+  char m_kChar;
+};
+
+//===----------------------------------------------------------------------===//
+// Token-Type Dictionary
+//===----------------------------------------------------------------------===//
+class AutoTenDictionary {
+ public:
+  AutoTenDictionary();
+  std::tuple<AutoTenTokenType, AutoTenTokenValue, int> Lookup(const std::string& name) const;
+  bool HaveToken(const std::string& name) const;
+
+ private:
+  void AddToken(std::string name, std::tuple<AutoTenTokenValue, AutoTenTokenType, int> TokenMeta);
+
+ private:
+  /// token name, token value, token type, precedence.
+  std::map<std::string, std::tuple<AutoTenTokenValue, AutoTenTokenType, int>> m_Dictionary;
 };
 
 //===----------------------------------------------------------------------===//
 // Lexer Main Class
 //===----------------------------------------------------------------------===//
+
+enum class AutoTenLexerState {
+  kNone = 0,
+  kEof,
+  kIdentifier,
+  kNumber,
+  kString,
+  kOperation,
+};
+
 class AutoTenLexer {
  public:
+  explicit AutoTenLexer(const std::string& SrcFileName);
+
+  inline AutoTenToken GetToken() const { return m_Token; }
+  AutoTenToken GetNextToken();
+
  private:
+  void GetNextChar();
+  char PeekChar();
+  void AddToBuffer(char c);
+  void ReduceBuffer();
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, std::string Name,
+                   int SymbolPrecedence);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, int8_t IntValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, int16_t IntValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, int32_t IntValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, int64_t IntValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, float FloatValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                   double FloatValue, std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, bool BoolValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc, char CharValue,
+                   std::string Name);
+  void CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                   const std::string& StrVale, std::string Name);
+
+  void HandleEOFState();
+  void HandleIdentifierState();
+  void HandleNumberState();
+  void HandleStringState();
+  void HandleOperationState();
+
+  /// TODO Preprocess need to binding.
+
+  inline Location GetTokenLocation() const { return Location(m_FileName, m_Line, m_Column); }
+  void HandleDigit();
+  void HandleFloat();
+
+  std::shared_ptr<std::string> m_FileName;
+  std::ifstream m_IfStream;
+  int m_Line;
+  int m_Column;
+  Location m_Loc;
+  char m_CurrentChar;
+  AutoTenLexerState m_State;
+  AutoTenToken m_Token;
+  AutoTenDictionary m_Dictionary;
+  std::string m_Buffer;
 };
 
 //===----------------------------------------------------------------------===//
--- src/nncv/compiler/Frontend/TenLangLexer.cpp
+++ src/nncv/compiler/Frontend/TenLangLexer.cpp
@@ -1,7 +1,338 @@
-#include "nncv/compiler/frontend/TenLangLexer.hpp"
+#include "nncv/compiler/Frontend/TenLangLexer.hpp"
+#include <stdexcept>
 
 namespace nncv {
 namespace compiler {
-namespace fronted {}
+namespace fronted {
+
+AutoTenDictionary::AutoTenDictionary() {
+  /// Operators
+  AddToken("=", std::make_tuple(AutoTenTokenValue::kEqual, AutoTenTokenType::kOperators, 2));
+  AddToken(">=",
+           std::make_tuple(AutoTenTokenValue::kGreaterOrEqual, AutoTenTokenType::kOperators, 2));
+  AddToken(">", std::make_tuple(AutoTenTokenValue::kGreaterThen, AutoTenTokenType::kOperators, 2));
+  AddToken("<=", std::make_tuple(AutoTenTokenValue::kLessOrEqual, AutoTenTokenType::kOperators, 2));
+  AddToken("<", std::make_tuple(AutoTenTokenValue::kLessThen, AutoTenTokenType::kOperators, 2));
+  AddToken("+", std::make_tuple(AutoTenTokenValue::kPlus, AutoTenTokenType::kOperators, 10));
+  AddToken("-", std::make_tuple(AutoTenTokenValue::kMinus, AutoTenTokenType::kOperators, 10));
+  AddToken("*", std::make_tuple(AutoTenTokenValue::kMultiplyOrDeReference,
+                                AutoTenTokenType::kOperators, 20));
+  AddToken("&", std::make_tuple(AutoTenTokenValue::kReference, AutoTenTokenType::kOperators, 20));
+  AddToken("/", std::make_tuple(AutoTenTokenValue::kDivide, AutoTenTokenType::kOperators, 20));
+
+  /// Delimiters
+  AddToken(",", std::make_tuple(AutoTenTokenValue::kComma, AutoTenTokenType::kDelimiter, -1));
+  AddToken(":", std::make_tuple(AutoTenTokenValue::kColon, AutoTenTokenType::kDelimiter, -1));
+  AddToken("(",
+           std::make_tuple(AutoTenTokenValue::kParentheseOpen, AutoTenTokenType::kDelimiter, -1));
+  AddToken(")",
+           std::make_tuple(AutoTenTokenValue::kParentheseClose, AutoTenTokenType::kDelimiter, -1));
+  AddToken("{", std::make_tuple(AutoTenTokenValue::kBracketOpen, AutoTenTokenType::kDelimiter, -1));
+  AddToken("}",
+           std::make_tuple(AutoTenTokenValue::kBracketClose, AutoTenTokenType::kDelimiter, -1));
+  AddToken("[",
+           std::make_tuple(AutoTenTokenValue::kSbracketOpen, AutoTenTokenType::kDelimiter, -1));
+  AddToken("]",
+           std::make_tuple(AutoTenTokenValue::kSbracketClose, AutoTenTokenType::kDelimiter, -1));
+  AddToken("<",
+           std::make_tuple(AutoTenTokenValue::kAngleBracketOpen, AutoTenTokenType::kDelimiter, -1));
+  AddToken(">", std::make_tuple(AutoTenTokenValue::kAngleBracketClose, AutoTenTokenType::kDelimiter,
+                                -1));
+  AddToken(".", std::make_tuple(AutoTenTokenValue::kPeriod, AutoTenTokenType::kDelimiter, -1));
+  AddToken(";", std::make_tuple(AutoTenTokenValue::kSemicolon, AutoTenTokenType::kDelimiter, -1));
+  AddToken("@", std::make_tuple(AutoTenTokenValue::kAt, AutoTenTokenType::kDelimiter, -1));
+
+  /// Keywords-logical
+  AddToken("and", std::make_tuple(AutoTenTokenValue::kAnd, AutoTenTokenType::kKeywords, 20));
+  AddToken("or", std::make_tuple(AutoTenTokenValue::kOr, AutoTenTokenType::kKeywords, 10));
+  AddToken("xor", std::make_tuple(AutoTenTokenValue::kXor, AutoTenTokenType::kKeywords, 10));
+  AddToken("not", std::make_tuple(AutoTenTokenValue::kNot, AutoTenTokenType::kKeywords, 40));
+  AddToken("<<", std::make_tuple(AutoTenTokenValue::kShl, AutoTenTokenType::kKeywords, 20));
+  AddToken(">>", std::make_tuple(AutoTenTokenValue::kShr, AutoTenTokenType::kKeywords, 20));
+  AddToken("%", std::make_tuple(AutoTenTokenValue::kMod, AutoTenTokenType::kKeywords, 20));
+
+  /// Keywords-control-flow
+  AddToken("if", std::make_tuple(AutoTenTokenValue::kIf, AutoTenTokenType::kKeywords, -1));
+  AddToken("nil", std::make_tuple(AutoTenTokenValue::kNil, AutoTenTokenType::kKeywords, -1));
+  AddToken("else", std::make_tuple(AutoTenTokenValue::kElse, AutoTenTokenType::kKeywords, -1));
+  AddToken("while", std::make_tuple(AutoTenTokenValue::kWhile, AutoTenTokenType::kKeywords, -1));
+  AddToken("do", std::make_tuple(AutoTenTokenValue::kDo, AutoTenTokenType::kKeywords, -1));
+  AddToken("->", std::make_tuple(AutoTenTokenValue::kReturnValue, AutoTenTokenType::kKeywords, -1));
+
+  /// Keywords function, struct, impl
+  AddToken("func", std::make_tuple(AutoTenTokenValue::kFunc, AutoTenTokenType::kKeywords, -1));
+  AddToken("struct", std::make_tuple(AutoTenTokenValue::kStruct, AutoTenTokenType::kKeywords, -1));
+  AddToken("impl", std::make_tuple(AutoTenTokenValue::kImpl, AutoTenTokenType::kKeywords, -1));
+
+  /// I/O
+  AddToken("read", std::make_tuple(AutoTenTokenValue::kRead, AutoTenTokenType::kKeywords, -1));
+  AddToken("write", std::make_tuple(AutoTenTokenValue::kWrite, AutoTenTokenType::kKeywords, -1));
+
+  /// Types
+  AddToken("var", std::make_tuple(AutoTenTokenValue::kVar, AutoTenTokenType::kKeywords, -1));
+  AddToken("string",
+           std::make_tuple(AutoTenTokenValue::kString, AutoTenTokenType::kTypeString, -1));
+  AddToken("Tensor",
+           std::make_tuple(AutoTenTokenValue::kTensor, AutoTenTokenType::kTypeTensor, -1));
+  AddToken("int8", std::make_tuple(AutoTenTokenValue::kInt8, AutoTenTokenType::kTypeInteger, -1));
+  AddToken("int16", std::make_tuple(AutoTenTokenValue::kInt16, AutoTenTokenType::kTypeInteger, -1));
+  AddToken("int32", std::make_tuple(AutoTenTokenValue::kInt32, AutoTenTokenType::kTypeInteger, -1));
+  AddToken("float32",
+           std::make_tuple(AutoTenTokenValue::kFloat32, AutoTenTokenType::kTypeReal, -1));
+  AddToken("float64",
+           std::make_tuple(AutoTenTokenValue::kFloat64, AutoTenTokenType::kTypeReal, -1));
+  AddToken("bool", std::make_tuple(AutoTenTokenValue::kBool, AutoTenTokenType::kTypeBool, -1));
+  AddToken("file", std::make_tuple(AutoTenTokenValue::kFile, AutoTenTokenType::kKeywords, -1));
+  AddToken("char", std::make_tuple(AutoTenTokenValue::kChar, AutoTenTokenType::kTypeChar, -1));
+}
+
+void AutoTenDictionary::AddToken(std::string name,
+                                 std::tuple<AutoTenTokenValue, AutoTenTokenType, int> TokenMeta) {
+  m_Dictionary.insert(std::pair<decltype(name), decltype(TokenMeta)>(name, TokenMeta));
+}
+
+std::tuple<AutoTenTokenType, AutoTenTokenValue, int> AutoTenDictionary::Lookup(
+    const std::string& name) const {
+  AutoTenTokenValue TokenValue = AutoTenTokenValue::kUnused;
+  AutoTenTokenType TokenType = AutoTenTokenType::kIdentifier;
+  int Precedence = -1;
+  auto Iter = m_Dictionary.find(name);
+
+  if (Iter != m_Dictionary.end()) {
+    TokenValue = std::get<0>(Iter->second);
+    TokenType = std::get<1>(Iter->second);
+    Precedence = std::get<2>(Iter->second);
+  }
+
+  return std::make_tuple(TokenType, TokenValue, Precedence);
+}
+
+bool AutoTenDictionary::HaveToken(const std::string& name) const {
+  auto Iter = m_Dictionary.find(name);
+
+  if (Iter != m_Dictionary.end()) { return true; }
+
+  return false;
+}
+
+AutoTenLexer::AutoTenLexer(const std::string& SrcFileName)
+    : m_FileName(std::make_shared<std::string>(SrcFileName)),
+      m_Line(1),
+      m_Column(0),
+      m_CurrentChar(0),
+      m_State(AutoTenLexerState::kNone) {
+  m_IfStream.open(*m_FileName);
+  if (m_IfStream.fail()) {
+    throw std::runtime_error("When trying to open" + (*m_FileName) + " Error Occurs.");
+  }
+}
+
+void AutoTenLexer::GetNextChar() {
+  m_CurrentChar = m_IfStream.get();
+
+  if (m_CurrentChar == '\n') {
+    m_Line++;
+    m_Column = 0;
+  } else {
+    m_Column++;
+  }
+}
+
+char AutoTenLexer::PeekChar() {
+  char c = m_IfStream.peek();
+  return c;
+}
+
+void AutoTenLexer::AddToBuffer(char c) { m_Buffer.push_back(c); }
+
+void AutoTenLexer::ReduceBuffer() { m_Buffer.pop_back(); }
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               std::string Name, int SymbolPrecedence) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, Name, SymbolPrecedence);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               int8_t IntValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, IntValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               int16_t IntValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, IntValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               int32_t IntValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, IntValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               int64_t IntValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, IntValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               float FloatValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, FloatValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               double FloatValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, FloatValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               bool BoolValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, BoolValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               char CharValue, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, CharValue, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+void AutoTenLexer::CreateToken(AutoTenTokenType Tt, AutoTenTokenValue Tv, const Location& Loc,
+                               const std::string& StrVale, std::string Name) {
+  m_Token = AutoTenToken(Tt, Tv, Loc, StrVale, Name);
+  m_Buffer.clear();
+  m_State = AutoTenLexerState::kNone;
+}
+
+AutoTenToken AutoTenLexer::GetNextToken() {
+  bool matched = false;
+  do {
+    if (m_State != AutoTenLexerState::kNone) { matched = true; }
+
+    switch (m_State) {
+      case AutoTenLexerState::kNone: GetNextChar(); break;
+      case AutoTenLexerState::kEof: HandleEOFState(); break;
+      case AutoTenLexerState::kIdentifier: HandleIdentifierState(); break;
+      case AutoTenLexerState::kNumber: HandleNumberState(); break;
+      case AutoTenLexerState::kString: HandleStringState(); break;
+      case AutoTenLexerState::kOperation: HandleOperationState(); break;
+      default: {
+        throw std::logic_error("When Lexer parse next token, lexer state mismatch.");
+      }
+    }
+    if (m_State == AutoTenLexerState::kNone) {
+      // TODO preprocess();
+      if (m_IfStream.eof()) {
+        m_State = AutoTenLexerState::kEof;
+      } else {
+        if (std::isalpha(m_CurrentChar) || (m_CurrentChar == '_')) {
+          m_State = AutoTenLexerState::kIdentifier;
+        } else if (std::isdigit(m_CurrentChar)) {
+          m_State = AutoTenLexerState::kNumber;
+        } else if (m_CurrentChar == '"') {
+          m_State = AutoTenLexerState::kString;
+        } else {
+          m_State = AutoTenLexerState::kOperation;
+        }
+      }
+    }
+  } while (!matched);
+
+  return m_Token;
+}
+
+void AutoTenLexer::HandleEOFState() {
+  m_Loc = GetTokenLocation();
+  CreateToken(AutoTenTokenType::kEof, AutoTenTokenValue::kUnused, m_Loc, std::string("EOF"), -1);
+  m_IfStream.close();
+}
+
+void AutoTenLexer::HandleNumberState() {
+  m_Loc = GetTokenLocation();
+  bool IsFloat = false;
+  bool IsExponent = false;
+
+  int NumberSystem = 10;
+
+  enum class NumberState { kInteger, kFloat, kExponent, kDone };
+  NumberState ThisNumberState = NumberState::kInteger;
+
+  do {
+    switch (ThisNumberState) {
+      case NumberState::kInteger: {
+        if (NumberSystem == 10) { HandleDigit(); }
+        break;
+      }
+      case NumberState::kFloat: {
+        HandleFloat();
+        IsFloat = true;
+        break;
+      }
+      case NumberState::kExponent: {
+      }
+      case NumberState::kDone: break;
+    }
+
+    if (m_CurrentChar == '.') {
+      if (IsFloat) {
+        // TODO Error float should just have one dots
+      }
+      if (IsExponent) {
+        // TODO Error Scientist number representation in AutoTen can not have dot.
+      }
+      if (NumberSystem != 10) {
+        // TODO Error Float only support number system 10.
+      }
+      ThisNumberState = NumberState::kFloat;
+    } else if (m_CurrentChar == 'E' || m_CurrentChar == 'e') {
+      if (IsExponent) {
+        // TODO Scientist presentation can not have more than one e / E
+      }
+      ThisNumberState = NumberState::kExponent;
+    } else {
+      ThisNumberState = NumberState::kDone;
+    }
+  } while (ThisNumberState != NumberState::kDone);
+  // TODO
+}
+
+void AutoTenLexer::HandleDigit() {
+  AddToBuffer(m_CurrentChar);
+  GetNextChar();
+
+  while (std::isdigit(m_CurrentChar)) {
+    AddToBuffer(m_CurrentChar);
+    GetNextChar();
+  }
+}
+
+void AutoTenLexer::HandleFloat() {
+  if (!std::isdigit(PeekChar())) {
+    // TODO Error "Fraction number part should be numbers");
+  }
+
+  AddToBuffer(m_CurrentChar);
+  GetNextChar();
+
+  while (std::isdigit(m_CurrentChar)) {
+    AddToBuffer(m_CurrentChar);
+    GetNextChar();
+  }
+}
+
+}  // namespace fronted
 }  // namespace compiler
 }  // namespace nncv
